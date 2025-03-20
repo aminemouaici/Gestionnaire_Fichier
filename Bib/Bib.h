c6552f050579538038cc762aeca37a6b08d2d0be
lettyz3 <155987572+lettyz3@users.noreply.github.com>
2025-03-20 11:12:30 +0100

    Update Bib.h

--- Bib/Bib.h
+++ Bib/Bib.h
@@ -45,7 +45,7 @@ typedef struct inode {
     char nom[32];
     uint8_t type;        // 0 = fichier, 1 = répertoire
     uint8_t ID;
-    uint8_t pére;              //ID du pére
+    uint8_t pere;              //ID du pére
     uint8_t permissions;
     union {
         fichier fichier;
